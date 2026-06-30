e880671d4c019c55a53542bebf3a4c93617ecd59
rabeezkhalid <266901330+rabeezkhalid@users.noreply.github.com>
2026-06-30 22:29:17 +0500

    Update Main.java

--- Main.java
+++ Main.java
@@ -2,26 +2,11 @@ public class Main {
 
     public static void main(String[] args) {
 
-        Graph graph = new Graph(7);
+        String text = "Insertion sort typically has a smaller constant factor than merge sort";
+        String pattern = "sort";
 
-        graph.addEdge(0,1,3);
-        graph.addEdge(0,3,3);
-        graph.addEdge(1,2,4);
-        graph.addEdge(2,0,3);
-        graph.addEdge(2,4,2);
-        graph.addEdge(2,3,1);
-        graph.addEdge(3,4,2);
-        graph.addEdge(3,5,6);
-        graph.addEdge(4,1,1);
-        graph.addEdge(4,6,1);
-        graph.addEdge(5,6,9);
+        BoyerMoore bm = new BoyerMoore();
 
-        EdmondsKarp ek = new EdmondsKarp(graph);
-
-        int maxFlow = ek.maxFlow(0,5);
-
-        System.out.println("Maximum Flow = " + maxFlow);
-
-        ek.printMinCut(0);
+        bm.search(text, pattern);
     }
 }
